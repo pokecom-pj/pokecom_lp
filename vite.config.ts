57b626fc52a2913ca55ef780d36c64b0105018fa
pokecom-pj <pokecom0001@gmail.com>
2025-06-12 05:46:31 +0000

    pages公開要コミット

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/pokecom_lp/",
   server: {
     host: true,
     port: 5173,
